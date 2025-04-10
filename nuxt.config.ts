e51d25c337c50c098605d74be4d0f11bcdc5bc27
descreetStudios <descreetop@gmail.com>
2025-04-10 10:48:32 +0200

    Aligned grid to vertical screen for tablet and phone

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+  },
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })
