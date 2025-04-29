727f0fe45c848dda83b94f24bcd7e2d5ec37e943
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2025-04-29 15:17:00 +0200

    Move navbar and transition to default.vue, code cleaning

--- pages/tutorial.vue
+++ pages/tutorial.vue
@@ -1,11 +1,4 @@
 <template>
-  <div ref="left" class="left">
-		<img src="@/assets/images/portone.jpg">
-	</div>
-	<div ref="right" class="right">
-		<img src="@/assets/images/portone.jpg">
-	</div>
-
 <!-- yap sesh -->
   <div class="page-wrapper">
     <div class="content">
@@ -55,20 +48,8 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    this.transitionOpen();
-  },
-  methods: {
-    transitionOpen() {
-      this.$refs.left.style.animation="leftOut 1s forwards";
-	    this.$refs.right.style.animation="rightOut 1s forwards";
-    },
-  }
-}
 </script>
 
 <style lang="scss">
-    @import url('../styles/transition.scss');
     @import url('../styles/tutorial.scss');
 </style>
