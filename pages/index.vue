f2332eba229f8f03051104fa2ab8d57d69b32231
ScalierBullet63 <199808687+ScalierBullet63@users.noreply.github.com>
2025-04-29 21:24:59 +0200

    Remove useless box shadow in title.scss, move title and buttons to default.vue from index.vue, navbar improvement, links fix, bugs fix

--- pages/index.vue
+++ pages/index.vue
@@ -9,25 +9,6 @@
 		</div>
 	</div>
 
-	<!-- Buttons and Title -->
-	<div class="title">
-		<img class="titleImage" src="@/assets/images/logo.png" alt="UPG">
-		<div class="buttons">
-
-			<button type="button" class="playButton" ref="button" @click="transitionClose(1)">
-				<span>PLAY</span>
-			</button>
-
-			<button type="button" ref="button" @click="transitionClose(2)">
-				<span>TUTORIAL</span>
-			</button>
-
-			<button type="button" ref="button" @click="transitionClose(3)">
-				<span>CREDITS</span>
-			</button>
-		</div>
-	</div>
-
 	<!--
 	<div class="bat"></div>
 	-->
@@ -75,8 +56,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('@/styles/title.scss');
-@import url('@/styles/bg.scss');
+// @import url('@/styles/title.scss');
+// @import url('@/styles/bg.scss');
 // @import url('@/styles/bat.scss');
 
 /* DEBUG
